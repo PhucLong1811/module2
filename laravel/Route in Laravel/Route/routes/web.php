4c3d8a5a75cc72f140aa96caeeb3a7318774d428
long <tinhanh473@gmail.com>
2019-02-28 08:48:35 +0700

    long

---
+++ laravel/Route in Laravel/Route/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    echo "<h2>This is Home page</h2>";
+});
+
+Route::get('/about', function () {
+    echo "<h2>This is About page</h2>";
+});
+
+Route::get('/contact', function () {
+    echo "<h2>This is Contact page</h2>";
+});
+Route::get('/user', function () {
+    return view('user', ['name'=>'Phúc Long']);
+});
+Route::get('/user', function () {
+    return view('user', ['name'=>'Phúc Long']);
+});
+Route::get('/user/Long', function ($name="Long") {
+    echo "<h2>User name is $name</h2>";
+});
+Route::get('/', 'HomeController@index');
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/{timezone?}', function ($timezone = null) {
+    if (!empty($timezone)) {
+
+        // Khởi tạo giá trị giờ theo múi giờ UTC
+        $time = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone('UTC'));
+
+        // Thay đổi về múi giờ được chọn
+        $time->setTimezone(new DateTimeZone(str_replace('-', '/', $timezone)));
+
+        // Hiển thị giờ theo định dạng mong muốn
+        echo 'Múi giờ bạn chọn ' . $timezone . ' hiện tại đang là: ' . $time->format('d-m-Y H:i:s');
+    }
+    return view('index');
+});
